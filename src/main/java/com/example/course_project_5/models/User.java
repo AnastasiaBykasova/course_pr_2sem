755302bc3a376bbfda4fdf2cdb3259a9f33baea7
AnastasiaBykasova <nastybykasova@gmail.com>
2022-07-11 21:49:45 +0300

    add user Model

---
+++ src/main/java/com/example/course_project_5/models/User.java
@@ -0,0 +1,46 @@
+package com.example.course_project_5.models;
+
+public class User {
+    private int id;
+    private String login;
+    private String password;
+    private UserStatus status;
+    private int level;
+
+    public User(int id,
+                String login,
+                String password,
+                UserStatus status,
+                int level
+    ) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.status = status;
+        this.level = level;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserStatus getStatus() {
+        return status;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+    public enum UserStatus {
+        Basic,
+        Super
+    }
+}
